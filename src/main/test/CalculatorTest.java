ff7d88d260cc167068d16c357ff399a9e6e8f8d4
Mykyta <nick90141@gmail.com>
2023-11-09 10:58:20 +0200

    ЗМІНЕНЕ

--- src/main/test/CalculatorTest.java
+++ src/main/test/CalculatorTest.java
@@ -1,19 +1,28 @@
 package Task_2;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
-    public void testCalculateSum() {
-        Calculator calculator = new Calculator();
-
-        // Позитивный тест
+    public void testCalculateSum_Positive() {
         assertEquals(5, calculator.calculateSum(2, 3));
+    }
 
-        // Тест на исключение
+    @Test
+    public void testCalculateSum_Exception() {
         try {
             calculator.calculateSum(-2, 3);
+            fail("Очікувалося, що виникне IllegalArgumentException");
         } catch (IllegalArgumentException e) {
             assertEquals("Оба числа должны быть неотрицательными", e.getMessage());
         }
